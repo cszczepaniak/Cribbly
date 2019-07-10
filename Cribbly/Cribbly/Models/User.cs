965a64c3aa0e9a67df7d4b315de746e07c853746
Adam Szczepaniak <aszczepaniak@atomicdatallc.onmicrosoft.com>
2019-07-10 11:50:42 -0500

    Re-scaffolded Identity to give us access to all View and Controller files

--- Cribbly/Cribbly/Models/User.cs
+++ Cribbly/Cribbly/Models/User.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Cribbly.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         private int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
     }
