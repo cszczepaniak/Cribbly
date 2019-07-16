c3a7b0f13eb6ff386155dc713da4bcea8fc56f76
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2019-07-15 20:59:17 -0500

    got application talking to MySQL DB

--- Cribbly/Cribbly/Startup.cs
+++ Cribbly/Cribbly/Startup.cs
@@ -35,7 +35,7 @@ namespace Cribbly
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
