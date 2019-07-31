0aa5bd79311c75e0d96a7912f68bd2a531b9029b
Adam Szczepaniak <aszczepaniak@atomicdatallc.onmicrosoft.com>
2019-07-31 15:44:37 -0500

    more standing

--- Cribbly/Cribbly/Controllers/StandingsController.cs
+++ Cribbly/Cribbly/Controllers/StandingsController.cs
@@ -24,7 +24,6 @@ namespace Cribbly.Controllers
         //View master list for all tournament standings
         public IActionResult GetAllStandings()
         {
-            //Get ApplicationDbContext
             //Find all standings 
             //Return all results to the view
             return View();
@@ -34,7 +33,6 @@ namespace Cribbly.Controllers
         [Authorize(Roles = "Admin")]
         public IActionResult CreateStandings()
         {
-            //Get ApplicationDbContext
             //Find all users where HasTeam = 0
             var teamlessUsers = _context.ApplicationUsers.Where(m => m.HasTeam == false).ToList();
             List<Team> newTeams = new List<Team>();
@@ -43,30 +41,49 @@ namespace Cribbly.Controllers
             //For all users with no team, pair them up with another user who also does not have a team
             for (var i = 0; i < teamlessUsers.Count; i++)
             {
-                string p1 = teamlessUsers[i].FirstName + " " + teamlessUsers[i].LastName;
-                string p2 = teamlessUsers[i + 1].FirstName + " " + teamlessUsers[i + 1].LastName;
-                //Seed random 6 digit numbers for Team Id
-                int id = rnd.Next(100000, 999999);
-                Team team = new Team()
+                try
                 {
-                    Id = id,
-                    Name = p1 + " / " + p2,
-                    PlayerOne = p1,
-                    PlayerTwo = p2
-                };
-                newTeams.Add(team);
-                teamlessUsers.RemoveRange(i, 2);
-                teamlessUsers[i].HasTeam = teamlessUsers[i + 1].HasTeam = true;
-                teamlessUsers[i].TeamId = teamlessUsers[i + 1].TeamId = id;
-                i = 0;
+                    string p1 = teamlessUsers[i].FirstName + " " + teamlessUsers[i].LastName;
+                    string p2 = teamlessUsers[i + 1].FirstName + " " + teamlessUsers[i + 1].LastName;
+
+                    //Seed random 6 digit numbers for Team Id
+                    int id = rnd.Next(100000, 999999);
+
+                    //Create new team 
+                    Team team = new Team()
+                    {
+                        Id = id,
+                        Name = p1 + " / " + p2,
+                        PlayerOne = p1,
+                        PlayerTwo = p2
+                    };
+
+                    //Add new team to List
+                    newTeams.Add(team);
+                    //Remove two users from teamlessPlayers List
+                    teamlessUsers.RemoveRange(i, 2);
+                    //Set user team attributes to reflect them now being teamed up
+                    teamlessUsers[i].HasTeam = teamlessUsers[i + 1].HasTeam = true;
+                    teamlessUsers[i].TeamId = teamlessUsers[i + 1].TeamId = id;
+                    //Reset i to 0 so we grab elements from the beginning of the list
+                    i = 0;
+                }
+                //List out of bounds - this means there is a user left over
+                catch
+                {
+                    //Alert admin that a user is left over and exit loop
+                    break;
+                }
+
 
             }
+
             _context.Teams.AddRange(newTeams);
             _context.SaveChanges();
-            //Throw up an alert and display left over user if we have an odd number of teamless players
 
             //Find all teams
             var allTeams = _context.Teams.ToList();
+
             //Populate all completed teams in the Standings table
             foreach (var team in allTeams)
             {
@@ -74,13 +91,13 @@ namespace Cribbly.Controllers
                 standing.TeamName = team.Name;
                 _context.Standings.Add(standing);
             }
-            return View();
+
+            return View(teamlessUsers);
         }
 
         //Submit a score to be added to the standings
         public IActionResult PostScore()
         {
-            //Get ApplicationDbContext
             //Find your team's standing
             //Fill in the appropriate properties depending on which game number is being posted
             return View();
