2d2b259b0801ca514aa504908e6bef5d8813ac96
cszczepaniak <cszczepaniak@gmail.com>
2019-08-03 23:05:26 -0500

    add skeleton of BracketController

---
+++ Cribbly/Cribbly/Controllers/BracketController.cs
@@ -0,0 +1,21 @@
+﻿using Cribbly.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cribbly.Controllers
+{
+    public class BracketController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BracketController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
