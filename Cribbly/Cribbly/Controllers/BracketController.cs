9ba984079f0e363fd86f199b2f7e48f39cfd179c
cszczepaniak <cszczepaniak@gmail.com>
2019-08-03 23:48:07 -0500

    start method to seed bracket, add private method to grab top 32 teams

--- Cribbly/Cribbly/Controllers/BracketController.cs
+++ Cribbly/Cribbly/Controllers/BracketController.cs
@@ -1,4 +1,5 @@
 ﻿using Cribbly.Data;
+using Cribbly.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,30 @@ namespace Cribbly.Controllers
             _context = context;
         }
 
+        public IActionResult SeedBracket(List<Standing> standings)
+        {
+            var bracketPool = GetBracketPool(standings);
+            return View();
+        }
+
+        private List<Standing> GetBracketPool(List<Standing> standings)
+        {
+            // Should this be hard coded?
+            var nTeams = 32;
+            var bracketTeams = new List<Standing>();
+            var divisions = standings.Select(s => s.Division).Distinct();
+            // First add the first team in each division to the bracket pool
+            foreach (var d in divisions)
+            {
+                var divisionStandings = standings.Where(s => s.Division.Equals(d)).OrderBy(s => s.TotalWins);
+                bracketTeams.Add(divisionStandings.First());
+                standings.Remove(divisionStandings.First());
+            }
+            // Then fill the rest of the pool with top overall remaining teams
+            var remaining = standings.OrderBy(s => s.TotalWinLoss);
+            bracketTeams.AddRange(remaining.Take(nTeams - bracketTeams.Count()));
+            return bracketTeams;
+        }
 
     }
 }
