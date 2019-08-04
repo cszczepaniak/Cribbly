1bfa33f20899cf6a884c794eebcfda9f14662a9d
cszczepaniak <cszczepaniak@gmail.com>
2019-08-04 00:30:14 -0500

    extract number of teams as a constant in this class

--- Cribbly/Cribbly/Controllers/BracketController.cs
+++ Cribbly/Cribbly/Controllers/BracketController.cs
@@ -12,6 +12,7 @@ namespace Cribbly.Controllers
     public class BracketController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private const int _numTeams = 32;
 
         public BracketController(ApplicationDbContext context)
         {
@@ -23,14 +24,12 @@ namespace Cribbly.Controllers
             // Get the pool of teams who made the cut
             var bracketPool = GetBracketPool(standings);
             // Add a seed to the sorted bracket pool members
-            bracketPool.Zip(Enumerable.Range(1, 32), (s, i) => s.Seed = i);
+            bracketPool.Zip(Enumerable.Range(1, _numTeams), (s, i) => s.Seed = i);
             return View();
         }
 
         private List<Standing> GetBracketPool(List<Standing> standings)
         {
-            // Should this be hard coded?
-            var nTeams = 32;
             var bracketTeams = new List<Standing>();
             var divisions = standings.Select(s => s.Division).Distinct();
             // First add the first team in each division to the bracket pool
@@ -42,7 +41,7 @@ namespace Cribbly.Controllers
             }
             // Then fill the rest of the pool with top overall remaining teams
             var remaining = standings.OrderBy(s => s.TotalWinLoss);
-            bracketTeams.AddRange(remaining.Take(nTeams - bracketTeams.Count()));
+            bracketTeams.AddRange(remaining.Take(_numTeams - bracketTeams.Count()));
             return bracketTeams.OrderBy(s => s.TotalWinLoss).ToList();
         }
 
