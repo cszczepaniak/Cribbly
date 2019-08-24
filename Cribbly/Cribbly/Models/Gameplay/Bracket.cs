ad418ffc8507352a7e8258dea22b49af37ceb2ec
cszczepaniak <cszczepaniak@gmail.com>
2019-08-24 12:39:41 -0500

    add bracket class

---
+++ Cribbly/Cribbly/Models/Gameplay/Bracket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cribbly.Models.Gameplay
+{
+    public class Bracket
+    {
+        public bool IsSeeded { get; set; }
+        public List<Standing> Teams { get; set; }
+    }
+}
