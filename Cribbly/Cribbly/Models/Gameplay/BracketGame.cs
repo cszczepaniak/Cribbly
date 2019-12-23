bb139b75f51bcb555e26479a7eb454bb30a98000
cszczepaniak <cszczepaniak@gmail.com>
2019-12-23 17:59:11 -0600

    add bracket game stuff

---
+++ Cribbly/Cribbly/Models/Gameplay/BracketGame.cs
@@ -0,0 +1,9 @@
+namespace Cribbly.Models.Gameplay
+{
+    public class BracketGame : Game
+    {
+        public int Round { get; set; }
+        public int Team1Seed { get; set; }
+        public int Team2Seed { get; set; }
+    }
+}
