e473221aab6edf7e81786c413615dafc8bc1caac
cszczepaniak <cszczepaniak@gmail.com>
2020-01-19 00:03:56 -0600

    rm unused models

--- Cribbly/Cribbly/Models/Gameplay/BracketGame.cs
+++
@@ -1,9 +0,0 @@
-namespace Cribbly.Models.Gameplay
-{
-    public class BracketGame : Game
-    {
-        public int Round { get; set; }
-        public int Team1Seed { get; set; }
-        public int Team2Seed { get; set; }
-    }
-}
