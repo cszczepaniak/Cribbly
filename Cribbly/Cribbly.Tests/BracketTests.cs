1edbe8dec9c02dc8111b6566190feaa8d8b9882c
cszczepaniak <cszczepaniak@gmail.com>
2020-02-01 17:41:08 -0600

    start to build up some tests

--- Cribbly/Cribbly.Tests/BracketTests.cs
+++ Cribbly/Cribbly.Tests/BracketTests.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Cribbly.Models;
+using Cribbly.Models.Gameplay;
 
 namespace Cribbly.Tests
 {
     public class BracketTests
     {
         [Fact]
+        public void TestSeedBracket()
+        {
+            var standings = getMockStandings();
+            var bracket = new Bracket(standings, 8);
+        }
+
+        [Fact]
         public void TestAdvance()
         {
 
         }
-        private Bracket makeBracket(int nTeams)
+        private List<Standing> getMockStandings()
         {
-            var standings = new List<Standing>()
+            return new List<Standing>()
             {
-                new Standing { Division="div0",TeamName="team0", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div0",TeamName="team1", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div0",TeamName="team2", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div0",TeamName="team3", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div0",TeamName="team4", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div1",TeamName="team5", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div1",TeamName="team6", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div1",TeamName="team7", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div1",TeamName="team8", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
-                new Standing { Division="div1",TeamName="team9", G1Score=121, G2Score=121, G3Score=121, G1WinLoss=20, G1WinLoss=20, G1WinLoss=20 },
+                new Standing { Division="div0",TeamName="team0", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div0",TeamName="team1", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div0",TeamName="team2", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div0",TeamName="team3", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div0",TeamName="team4", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div1",TeamName="team5", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div1",TeamName="team6", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div1",TeamName="team7", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div1",TeamName="team8", G1Score=121, G2Score=121, G3Score=121 },
+                new Standing { Division="div1",TeamName="team9", G1Score=121, G2Score=121, G3Score=121 },
             };
         }
     }
