001a905d076936e33b1e71f3f70da79c2b309b45
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2020-03-01 04:30:00 -0600

    extending postscore model

--- Cribbly/Cribbly/Models/ViewModels/PostScoreView.cs
+++ Cribbly/Cribbly/Models/ViewModels/PostScoreView.cs
@@ -8,6 +8,9 @@ namespace Cribbly.Models.ViewModels
 {
     public class PostScoreView
     {
+        public int Game1Score { get; set; }
+        public int Game2Score { get; set; }
+        public int Game3Score { get; set; }
         public int GameNumber { get; set; }
         public int TeamId { get; set; }
         public string TeamName { get; set; }
