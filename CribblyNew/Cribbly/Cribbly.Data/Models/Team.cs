e4525d3b0beb21968031e72be0d85061952399e8
cszczepaniak <cszczepaniak@gmail.com>
2020-03-13 11:49:13 -0500

    add Team data model

---
+++ CribblyNew/Cribbly/Cribbly.Data/Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cribbly.Data.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Player1ID { get; set; }
+        [Required]
+        public int Player2ID { get; set; }
+        [Range(0, 121)]
+        public int Game1Score { get; set; }
+        [Range(0, 121)]
+        public int Game2Score { get; set; }
+        [Range(0, 121)]
+        public int Game3Score { get; set; }
+    }
+}
