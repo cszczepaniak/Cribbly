d2e52c4497eee6e3e58a9336199086dc84b53d7c
cszczepaniak <cszczepaniak@gmail.com>
2020-03-13 11:35:21 -0500

    start to organize things

--- CribblyNew/Cribbly/Startup.cs
+++ CribblyNew/Cribbly/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Cribbly.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Cribbly
 {
@@ -29,16 +24,7 @@ namespace Cribbly
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.ConfigureExceptionHandling(env);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -46,12 +32,7 @@ namespace Cribbly
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.ConfigureEndpoints();
         }
     }
 }
