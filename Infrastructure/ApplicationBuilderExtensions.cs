bf252b12d9557a210b5b768399d246c1bb356ccb
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2020-03-15 14:24:13 -0500

    clean up folder structure

---
+++ Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Cribbly.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ConfigureExceptionHandling(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+        }
+        public static void ConfigureEndpoints(this IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
