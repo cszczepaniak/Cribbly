a5202f2a3195b6566205cc5d4282d4aa6dad8e24
cszczepaniak <cszczepaniak@gmail.com>
2020-05-05 21:32:43 -0500

    add mvc

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,10 @@ namespace Cribbly
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +29,9 @@ namespace Cribbly
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(options =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                options.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
