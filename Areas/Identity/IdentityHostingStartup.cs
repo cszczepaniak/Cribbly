c4a058627553a745f281f1e6fdd2fb8807928c1d
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 10:08:50 -0500

    move dbcontext to data namespace

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -1,6 +1,6 @@
 using System;
 using Cribbly.Models;
-using Cribbly.Services;
+using Cribbly.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Cribbly.Services
+namespace Cribbly.Data
 {
     public class AppDbContext : IdentityDbContext<Player>
     {
@@ -19,6 +19,13 @@ namespace Cribbly.Services
             modelBuilder.Entity<Player>()
                 .HasOne<Team>(p => p.Team)
                 .WithMany(t => t.Players);
+
+            modelBuilder.Entity<TeamPlayInGame>()
+                .HasKey(t => new { t.TeamID, t.PlayInGameID });
+
+            modelBuilder.Entity<TeamPlayInGame>()
+                .HasOne(tg => tg.Team)
+                .WithMany(t => )
         }
 
         public DbSet<Team> Teams { get; set; }
