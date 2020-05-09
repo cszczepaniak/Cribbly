da832946c418a706d4b99d4a89b263d5fb12b771
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 11:32:30 -0500

    add some data annotations

--- Models/PlayInGame.cs
+++ Models/PlayInGame.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cribbly.Models
 {
     public class PlayInGame
     {
         public int ID { get; set; }
+        [MinLength(0)]
+        [MaxLength(3)]
         public List<TeamPlayInGame> TeamPlayInGames { get; set; }
+        [MinLength(2)]
+        [MaxLength(2)]
         public List<int> Scores { get; set; }
     }
 }
--- Models/Team.cs
+++ Models/Team.cs
@@ -1,12 +1,20 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cribbly.Models
 {
     public class Team
     {
         public int ID { get; set; }
+        [StringLength(255)]
         public string Name { get; set; }
+
+        [MinLength(2)]
+        [MaxLength(2)]
         public List<Player> Players { get; set; }
+
+        [MinLength(0)]
+        [MaxLength(3)]
         public List<TeamPlayInGame> TeamPlayInGames { get; set; }
     }
 }
