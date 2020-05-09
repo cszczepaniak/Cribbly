cd2eeb6e4a83926a1ac48a4ccdca9aa0f5d92e96
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 10:16:08 -0500

    migrate db

--- Migrations/20200509151438_TeamsAndPlayInGames.Designer.cs
+++ Migrations/20200509151438_TeamsAndPlayInGames.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Cribbly.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cribbly.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200509151438_TeamsAndPlayInGames")]
+    partial class TeamsAndPlayInGames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200509151438_TeamsAndPlayInGames.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cribbly.Migrations
+{
+    public partial class TeamsAndPlayInGames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayInGame",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Team1Score = table.Column<int>(nullable: false),
+                    Team2Score = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayInGame", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamPlayInGame",
+                columns: table => new
+                {
+                    TeamID = table.Column<int>(nullable: false),
+                    PlayInGameID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamPlayInGame", x => new { x.TeamID, x.PlayInGameID });
+                    table.ForeignKey(
+                        name: "FK_TeamPlayInGame_PlayInGame_PlayInGameID",
+                        column: x => x.PlayInGameID,
+                        principalTable: "PlayInGame",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeamPlayInGame_Teams_TeamID",
+                        column: x => x.TeamID,
+                        principalTable: "Teams",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamPlayInGame_PlayInGameID",
+                table: "TeamPlayInGame",
+                column: "PlayInGameID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamPlayInGame");
+
+            migrationBuilder.DropTable(
+                name: "PlayInGame");
+        }
+    }
+}
