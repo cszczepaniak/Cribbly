6fd7a99e931e00eb615d304fa15d187d183517fc
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 11:56:27 -0500

    migrate db

---
+++ Migrations/20200509165605_AddTourneyDataToTeam.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cribbly.Migrations
+{
+    public partial class AddTourneyDataToTeam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TournamentRound",
+                table: "Teams",
+                nullable: false,
+                defaultValue: -1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TournamentSeed",
+                table: "Teams",
+                nullable: false,
+                defaultValue: -1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TournamentRound",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "TournamentSeed",
+                table: "Teams");
+        }
+    }
+}
