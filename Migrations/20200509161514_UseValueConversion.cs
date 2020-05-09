9cef58d999370023eb226aa6c36408f26b9b2985
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 11:21:10 -0500

    migrate db

---
+++ Migrations/20200509161514_UseValueConversion.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cribbly.Migrations
+{
+    public partial class UseValueConversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Team1Score",
+                table: "PlayInGame");
+
+            migrationBuilder.DropColumn(
+                name: "Team2Score",
+                table: "PlayInGame");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Scores",
+                table: "PlayInGame",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Scores",
+                table: "PlayInGame");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Team1Score",
+                table: "PlayInGame",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Team2Score",
+                table: "PlayInGame",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
