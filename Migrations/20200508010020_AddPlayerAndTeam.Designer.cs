c1a27844083c1a86dc8edc7b2548e9942ebc6296
cszczepaniak <cszczepaniak@gmail.com>
2020-05-09 09:48:22 -0500

    migrate the db

--- Migrations/20200508010020_AddPlayerAndTeam.Designer.cs
+++ Migrations/20200508010020_AddPlayerAndTeam.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Cribbly.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cribbly.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200508010020_AddPlayerAndTeam")]
+    partial class AddPlayerAndTeam
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
