b32fb86e09b4c38406375276684fd28e09316a86
cszczepaniak <cszczepaniak@gmail.com>
2020-05-11 21:21:09 -0500

    make some helper props

--- Models/PlayInGame.cs
+++ Models/PlayInGame.cs
@@ -1,14 +1,29 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Cribbly.Models
 {
     public class PlayInGame
     {
         public int ID { get; set; }
+
+        // TeamPlayInGames represents the many-to-many relationship between teams and play in games
         [MinLength(0)]
         [MaxLength(3)]
         public List<TeamPlayInGame> TeamPlayInGames { get; set; }
+        // PlayInGames is a helper prop to grab the teams from the join table
+        public List<Team> Teams
+        {
+            get => TeamPlayInGames.Select(tg => tg.Team).ToList();
+            set => TeamPlayInGames = value.Select(v => new TeamPlayInGame()
+            {
+                TeamID = v.ID
+            }).ToList();
+        }
+
+
+        // TODO will the indices of scores and teams align properly? i.e. will team 0's score be at index 0?
         [MinLength(2)]
         [MaxLength(2)]
         [Range(0, 121)]
--- Models/Team.cs
+++ Models/Team.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Cribbly.Models
 {
@@ -13,10 +14,19 @@ namespace Cribbly.Models
         [MaxLength(2)]
         public List<Player> Players { get; set; }
 
+        // TeamPlayInGames represents the many-to-many relationship between teams and play in games
         [MinLength(0)]
         [MaxLength(3)]
         public List<TeamPlayInGame> TeamPlayInGames { get; set; }
-
+        // PlayInGames is a helper prop to grab the play in games from the join table
+        public List<PlayInGame> PlayInGames
+        {
+            get => TeamPlayInGames.Select(tg => tg.PlayInGame).ToList();
+            set => TeamPlayInGames = value.Select(v => new TeamPlayInGame()
+            {
+                PlayInGameID = v.ID
+            }).ToList();
+        }
         public Division Division { get; set; }
         public int TournamentSeed { get; set; }
         public int TournamentRound { get; set; }
