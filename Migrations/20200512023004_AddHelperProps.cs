d84048bce848c1b590a5e73d0b704bc9e1bcf21c
cszczepaniak <cszczepaniak@gmail.com>
2020-05-11 21:31:58 -0500

    migrate the db

---
+++ Migrations/20200512023004_AddHelperProps.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cribbly.Migrations
+{
+    public partial class AddHelperProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
