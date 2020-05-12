54c50586b4ddd186c8ced2948089d499882404aa
cszczepaniak <cszczepaniak@gmail.com>
2020-05-11 21:46:30 -0500

    migrate the db

--- Migrations/20200512024555_RemovePhoneNumberData.Designer.cs
+++ Migrations/20200512024555_RemovePhoneNumberData.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Cribbly.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cribbly.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200512024555_RemovePhoneNumberData")]
+    partial class RemovePhoneNumberData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
