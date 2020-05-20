191c8672042d2f50848d42cca5e480b7785dbba7
cszczepaniak <cszczepaniak@gmail.com>
2020-05-20 16:55:58 -0500

    start to add some tests

---
+++ Cribbly.Tests/Services/PlayerServiceTests.cs
@@ -0,0 +1,15 @@
+using Cribbly;
+using Cribbly.Services;
+using Xunit;
+
+namespace Cribbly.Tests.Services
+{
+    public class PlayerServiceTests
+    {
+        private PlayerService playerService;
+        public PlayerServiceTests(PlayerService playerService)
+        {
+            this.playerService = playerService;
+        }
+    }
+}
