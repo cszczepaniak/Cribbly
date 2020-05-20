ed2764b60b694711678b49dd8d825ff6d6e7ca2f
cszczepaniak <cszczepaniak@gmail.com>
2020-05-19 19:13:10 -0500

    flesh out the team service with new data models

--- Services/TeamService.cs
+++ Services/TeamService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cribbly.Data;
 using Cribbly.Models;
 
 namespace Cribbly.Services
@@ -16,14 +17,15 @@ namespace Cribbly.Services
         }
         public async Task<Team> CreateTeamAsync(Player p1, Player p2)
         {
-            var team = new Team()
+            var team = new TeamDataModel()
             {
-                Players = new List<Player>() { p1, p2 }
+                Player1ID = p1.Id,
+                Player2ID = p2.Id,
             };
-            await db.Teams.AddAsync(team);
-            return team;
+            await db.TeamDataModels.AddAsync(team);
+            return GetTeamWithPlayer(p1);
         }
-        // TODO maybe this should be in PlayInGameService?
+        // TODO maybe this should be in PlayInGameService? :nods:
         public async void ReportWinAsync(Team winner, Team loser, int loserScore)
         {
             if (loserScore < 0 || loserScore > Constants.WinningScore - 1)
@@ -46,7 +48,41 @@ namespace Cribbly.Services
             await db.PlayInGames.AddAsync(game);
         }
 
-        public Team GetTeamByID(int id) =>
-            db.Teams.Where(t => t.ID == id).FirstOrDefault();
+        public Team GetTeamWithPlayer(Player p)
+        {
+            var team = db.TeamDataModels.Where(tdm => tdm.Player1ID.Equals(p.Id) || tdm.Player2ID.Equals(p.Id)).FirstOrDefault();
+            return teamFromDataModel(team);
+        }
+
+        public Team GetTeamByID(int id)
+        {
+            var team = db.TeamDataModels.Where(tdm => tdm.ID == id).FirstOrDefault();
+            if (team == null)
+            {
+                throw new Exception("Team not found.");
+            }
+            return teamFromDataModel(team);
+        }
+
+        private Team teamFromDataModel(TeamDataModel team)
+        {
+            // TODO make a player service to fetch these instead of doing it below
+            var p1 = db.Users.Where(p => p.Id == team.Player1ID).FirstOrDefault();
+            var p2 = db.Users.Where(p => p.Id == team.Player2ID).FirstOrDefault();
+            // TODO make a game service to fetch these instead of doing it below
+            var g1 = db.PlayInGames.Where(g => g.ID == team.PlayInGame1ID).FirstOrDefault();
+            var g2 = db.PlayInGames.Where(g => g.ID == team.PlayInGame2ID).FirstOrDefault();
+            var g3 = db.PlayInGames.Where(g => g.ID == team.PlayInGame3ID).FirstOrDefault();
+            return new Team()
+            {
+                ID = team.ID,
+                Name = team.Name,
+                Players = new List<Player> { p1, p2 },
+                PlayInGames = new List<PlayInGame>() { g1, g2, g3 },
+                Division = team.Division,
+                TournamentSeed = team.TournamentSeed,
+                TournamentRound = team.TournamentRound,
+            };
+        }
     }
 }
