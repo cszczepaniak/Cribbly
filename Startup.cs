d736a307b415310789a0af9a414b1a5833640688
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2020-05-20 22:45:30 -0500

    added runtime compilation to app, more building of _guestHome.cshtml

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,8 @@ namespace Cribbly
             {
                 options.EnableEndpointRouting = false;
             });
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
